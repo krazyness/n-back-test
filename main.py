bc818a5dfbfe8f1b40c0823173a250860a20b8a8
Brody Dai <brodyxdai@gmail.com>
2023-07-03 14:36:27 -0400

    Add: sound, wider UI, better randomization system

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 import random
-import time
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
+from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 
+TRIALS = 25
+COUNTDOWN = 3
+
+LETTER_APPEAR_DURATION_SECONDS = 0.75
+PAUSE_DURATION_SECONDS = 2
+REPEAT_PROBABILITY = 0.4  # Probability of match
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,30 +18,39 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Memory Test")
         self.resize(700, 500)
 
-        self.countdown_time = 4
+        self.is_clicked = False
+        self.is_letter_appeared = False
+        self.is_practice = True
 
-        self.trials = 25  # Can be modified to your liking
+        self.correct_cnt = 0
+        self.incorrect_cnt = 0
+        self.missed_cnt = 0
+        self.trials_with_match_cnt = 0
+        self.trials_with_no_match_cnt = 0
 
-        self.clicked = False
-        self.letterAppeared = False
-        self.verify = False
-        self.practiceRunCheck = True
+        self.universal_letter = None
+        self.first_letter = None
+        self.second_letter = None
 
-        self.correct = 0
-        self.incorrect = 0
-        self.missed = 0
-        self.trialsWithMatch = 0
-        self.trialsWithNoMatch = 0
+        self.correct_sound_output = QAudioOutput()
+        self.correct_sound_output.setVolume(50)
 
-        self.universalLetter = "nothing"
-        self.first_letter = "nothing"
-        self.second_letter = "nothing"
+        self.incorrect_sound_output = QAudioOutput()
+        self.incorrect_sound_output.setVolume(50)
+
+        self.correct_sound = QMediaPlayer()
+        self.correct_sound.setAudioOutput(self.correct_sound_output)
+        self.correct_sound.setSource(QUrl.fromLocalFile("RightChoice.wav"))
+
+        self.incorrect_sound = QMediaPlayer()
+        self.incorrect_sound.setAudioOutput(self.incorrect_sound_output)
+        self.incorrect_sound.setSource(QUrl.fromLocalFile("WrongChoice.wav"))
 
         self.beginInfo = QPushButton("Click this button to start", self)
         self.beginInfo.setGeometry(200, 100, 300, 300)
         self.beginInfo.setStyleSheet("background-color: rgb(0, 255, 0);")
         self.beginInfoFont = QFont()
-        self.beginInfoFont.setPointSize(20)
+        self.beginInfoFont.setPointSize(16)
         self.beginInfo.setFont(self.beginInfoFont)
         self.beginInfo.clicked.connect(self.info_click)
 
@@ -48,19 +63,20 @@ class MainWindow(QMainWindow):
         self.center_button.clicked.connect(self.on_click)
         self.center_button.setVisible(False)
 
-        self.practice_help1 = QPushButton("1", self)
+        self.practice_help1 = QPushButton("", self)
         self.practice_help1.setGeometry(175, 200, 100, 100)
         self.practice_help1.setFont(self.center_buttonFont)
         self.practice_help1.setVisible(False)
         self.practice_help1.setEnabled(False)
 
-        self.practice_help2 = QPushButton("2", self)
+        self.practice_help2 = QPushButton("", self)
         self.practice_help2.setGeometry(50, 200, 100, 100)
         self.practice_help2.setFont(self.center_buttonFont)
         self.practice_help2.setVisible(False)
         self.practice_help2.setEnabled(False)
 
-        self.rounds = self.trials
+        self.rounds_cnt = 0
+        self.countdown_cnt = 0
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.update)
@@ -87,15 +103,29 @@ class MainWindow(QMainWindow):
         self.info.setVisible(True)
 
         self.info2 = QLabel('If you saw the same letter 2 trials ago, '
-                            'you click\nit with the mouse.', self)
-        self.info2.setGeometry(100, 10, 500, 500)
+                            'you click\nit with the mouse.'
+                            '\n\nIf correct, you will hear this sound:'
+                            '\n\nIf incorrect, you will hear this sound:', self)
+        self.info2.setGeometry(100, 40, 500, 500)
         self.info2Font = QFont("Arial", 15)
         self.info2.setFont(self.info2Font)
         self.info2.setVisible(True)
 
+        self.play_correct_button = QPushButton("Click here to play CORRECT sound", self)
+        self.play_correct_button.setGeometry(410, 275, 250, 45)
+        self.play_correct_button.setStyleSheet("background-color: rgb(0, 255, 0);")
+        self.play_correct_button.setVisible(True)
+        self.play_correct_button.clicked.connect(self.correct_sound.play)
+
+        self.play_incorrect_button = QPushButton("Click here to play INCORRECT sound", self)
+        self.play_incorrect_button.setGeometry(425, 330, 250, 45)
+        self.play_incorrect_button.setStyleSheet("background-color: rgb(252, 61, 61);")
+        self.play_incorrect_button.setVisible(True)
+        self.play_incorrect_button.clicked.connect(self.incorrect_sound.play)
+
         self.nextButton = QPushButton('Click here for the '
                                       'next info screen', self)
-        self.nextButton.setGeometry(100, 325, 220, 50)
+        self.nextButton.setGeometry(100, 390, 250, 50)
         self.nextButton.setStyleSheet("background-color: rgb(0, 255, 0);")
         self.nextButtonFont1 = QFont()
         self.nextButtonFont1.setPointSize(10)
@@ -105,7 +135,8 @@ class MainWindow(QMainWindow):
         self.nextButton.clicked.connect(self.info2_click)
 
     def info2_click(self):
-        self.info2.setVisible(False)
+        self.play_correct_button.setVisible(False)
+        self.play_incorrect_button.setVisible(False)
 
         self.info.setText('To help you learn this task, '
                           'at first there is a very easy\nversion. '
@@ -118,16 +149,23 @@ class MainWindow(QMainWindow):
                           '\nbased on your memory.')
         self.info.setGeometry(100, -75, 500, 500)
 
-        self.nextButton.setGeometry(100, 260, 225, 50)
+        self.info2.setText('Tip: Keep your cursor at '
+                           'the center button\nso that '
+                           'you won\'t miss a click.')
+
+        self.nextButton.setGeometry(100, 350, 250, 50)
         self.nextButton.setText("Click here to begin your practice run!")
         self.nextButton.clicked.connect(self.on_click)
 
     def info3_click(self):
-        self.trialsWithMatch = 0
-        self.trialsWithNoMatch = 0
-        self.correct = 0
-        self.missed = 0
-        self.incorrect = 0
+        self.trials_with_match_cnt = 0
+        self.trials_with_no_match_cnt = 0
+        self.correct_cnt = 0
+        self.missed_cnt = 0
+        self.incorrect_cnt = 0
+
+        self.countdown_cnt = 0
+        self.rounds_cnt = 0
 
         self.infoTitle.setVisible(True)
         self.resultButton.setVisible(False)
@@ -150,7 +188,7 @@ class MainWindow(QMainWindow):
         self.nextButton.clicked.connect(self.on_click)
 
     def on_click(self):
-        if self.countdown_time > 1:  # Initial Click
+        if self.countdown_cnt < COUNTDOWN:  # Initial Click
             self.center_button.setEnabled(False)
             self.center_button.setVisible(True)
 
@@ -159,99 +197,117 @@ class MainWindow(QMainWindow):
             self.infoTitle.setVisible(False)
             self.info2.setVisible(False)
 
-            self.correct = 0
-            self.incorrect = 0
-            self.missed = 0
+            self.correct_cnt = 0
+            self.incorrect_cnt = 0
+            self.missed_cnt = 0
 
             self.timer.start(1000)
         else:
-            self.clicked = True
+            self.is_clicked = True
 
     def update(self):
-        if self.countdown_time <= 1:  # Update when countdown finished/finishes
+        if self.countdown_cnt >= COUNTDOWN:  # Update when countdown finished/finishes
             self.center_button.setEnabled(True)
             self.timer.stop()
-            if self.rounds >= 1:
-                if not self.letterAppeared:  # Make Letter Appear
-                    self.letterAppeared = True
+            if self.rounds_cnt < TRIALS:
+                if not self.is_letter_appeared:  # Make Letter Appear
+                    self.is_letter_appeared = True
 
                     self.provide_random_letter()
-                    self.timer.start(750)
+                    self.timer.start(LETTER_APPEAR_DURATION_SECONDS * 1000)
                 else:  # Make Letter Not Appear And Check
-                    self.rounds -= 1
-                    self.check()
+                    self.rounds_cnt += 1
+                    self.center_button.setVisible(False)
 
-                    if self.clicked and self.verify:
-                        self.correct += 1
-                    elif not self.clicked and self.verify:
-                        self.missed += 1
-                    elif self.clicked and not self.verify:
-                        self.incorrect += 1
+                    if self.check():
+                        if self.is_clicked:
+                            self.correct_cnt += 1
+                            self.correct_sound.play()
+                        elif not self.is_clicked:
+                            self.incorrect_cnt += 1
+                            self.incorrect_sound.play()
+                    else:
+                        if self.is_clicked:
+                            self.incorrect_cnt += 1
+                            self.incorrect_sound.play()
+                        elif not self.is_clicked:
+                            pass
 
                     self.second_letter = self.first_letter
-                    self.first_letter = self.universalLetter
+                    self.first_letter = self.universal_letter
 
-                    self.letterAppeared = False
-                    self.clicked = False
-
-                    self.center_button.setVisible(False)
+                    self.is_letter_appeared = False
+                    self.is_clicked = False
+    
                     self.practice_help1.setVisible(False)
                     self.practice_help2.setVisible(False)
 
-                    self.timer.start(2000)
+                    self.timer.start(PAUSE_DURATION_SECONDS * 1000)
             else:
-                self.letterAppeared = False
+                self.is_letter_appeared = False
                 self.results()
         else:  # Update countdown
-            self.countdown_time -= 1
-            self.center_button.setText("{}".format(self.countdown_time))
+            self.center_button.setText(f"{COUNTDOWN-self.countdown_cnt}")
+            self.countdown_cnt += 1
 
     def provide_random_letter(self):
         letter = chr(random.randint(65, 65 + 7))
-        self.universalLetter = letter
-        self.center_button.setText(letter)
+        if self.first_letter == self.second_letter and self.first_letter is not None:
+            while True:
+                if letter == self.first_letter:
+                    letter = chr(random.randint(65, 65 + 7))
+                else:
+                    self.universal_letter = letter
+                    self.center_button.setText(letter)
+                    break
+        elif self.second_letter is not None:
+            if random.random() <= REPEAT_PROBABILITY:
+                self.universal_letter = self.second_letter
+                self.center_button.setText(self.second_letter)
+            else:
+                self.universal_letter = letter
+                self.center_button.setText(letter)
+        else:
+            self.universal_letter = letter
+            self.center_button.setText(letter)
 
-        if self.practiceRunCheck:
-            self.practiceRunFunc()
+        if self.is_practice:
+            if self.first_letter is not None:
+                self.practice_help1.setText(f"{self.first_letter}")
+                self.practice_help1.setVisible(True)
+            if self.second_letter is not None:
+                self.practice_help2.setText(f"{self.second_letter}")
+                self.practice_help2.setVisible(True)
 
         self.center_button.setVisible(True)
 
-    def practiceRunFunc(self):
-        if self.first_letter != "nothing":
-            self.practice_help1.setText("{}".format(self.first_letter))
-            self.practice_help1.setVisible(True)
-        if self.second_letter != "nothing":
-            self.practice_help2.setText("{}".format(self.second_letter))
-            self.practice_help2.setVisible(True)
-
     def check(self):
-        if self.universalLetter == self.second_letter:
-            self.verify = True
-            self.trialsWithMatch += 1
+        if self.universal_letter == self.second_letter:
+            self.trials_with_match_cnt += 1
+            return True
         else:
-            self.verify = False
-            self.trialsWithNoMatch += 1
+            self.trials_with_no_match_cnt += 1
+            return False
 
     def results(self):
         self.center_button.setVisible(False)
 
-        self.rounds = self.trials
-        self.countdown_time = 4
+        self.rounds_cnt = 0
 
-        self.universalLetter = "nothing"
-        self.first_letter = "nothing"
-        self.second_letter = "nothing"
+        self.universal_letter = None
+        self.first_letter = None
+        self.second_letter = None
         self.center_button.setText("Loading...")
 
-        self.info.setText('There were 25 trials total in this block.'
+        self.info.setText(f'There were {TRIALS} trials total in this block.'
                           '\n\nTotal trials that had a '
-                          f'match: {self.trialsWithMatch}'
+                          f'match: {self.trials_with_match_cnt}'
                           '\n\nTotal trials that had no '
-                          f'match: {self.trialsWithNoMatch}'
+                          f'match: {self.trials_with_no_match_cnt}'
                           '\n\nNumber of correctly matched '
-                          f'items: {self.correct}'
-                          f'\n\nNumber of missed items: {self.missed}'
-                          f'\n\nNumber of false alarms: {self.incorrect}')
+                          f'items: {self.correct_cnt}'
+                          f'\n\nNumber of missed items: {self.missed_cnt}'
+                          f'\n\nNumber of false alarms: {self.incorrect_cnt}')
         self.info.setVisible(True)
 
         self.resultButton = QPushButton("Click here to continue", self)
@@ -263,7 +319,7 @@ class MainWindow(QMainWindow):
         self.resultButton.setVisible(True)
         self.resultButton.clicked.connect(self.info3_click)
 
-        self.practiceRunCheck = False
+        self.is_practice = False
 
 
 if __name__ == "__main__":
